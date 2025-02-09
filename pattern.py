49622e90fcaae1b050eb4321ab163941faf8738a
markg <markgrahamsmith@hotmail.com>
2025-02-09 17:47:59 +0100

    added distribution methods

--- pattern.py
+++ pattern.py
@@ -23,9 +23,20 @@ class Pattern:
         for slice in self.slices:
             slice.display()
 
-    def exapand(self):
-        for slice in self.slices:
-            print(slice)
+    def expand(self):
+        for index, slice in enumerate(self.slices, start=1):
+            print(f"Slice {index}: {slice}")
+
+    def check_distribution(self):
+        total_distribution = sum(slice.distribution + slice.start_distribution for slice in self.slices)
+        return total_distribution == 1
+
+    def distribute_remaining(self):
+        total_distribution = sum(slice.distribution + slice.start_distribution for slice in self.slices)
+        if (total_distribution < 1):
+            remaining = 1 - total_distribution
+            for slice in self.slices:
+                slice.distribution += remaining/len(self.slices)
 
 class PatternSlice:
     def __init__(self, distribution, duration, start_distribution):
@@ -38,3 +49,22 @@ class PatternSlice:
 
     def display(self):
         print(f"PatternSlice: (Duration: {self.duration}, Distribution: {self.distribution}, Start Distribution: {self.start_distribution})")
+
+    def __str__(self):
+        return f"PatternSlice: (Duration: {self.duration}, Distribution: {self.distribution}, Start Distribution: {self.start_distribution})"
+
+def main():
+    pattern = Pattern()
+    pattern.add_slice(PatternSlice(0.2, 90, 0.15))
+    pattern.add_slice(PatternSlice(0.15, 90, 0))
+    pattern.add_slice(PatternSlice(0.1, 90, 0))
+    pattern.add_slice(PatternSlice(0.15, 90, 0))
+
+    pattern.expand()
+    print("Distribution check:", pattern.check_distribution())
+    pattern.distribute_remaining()
+    pattern.expand()
+    print("Distribution check:", pattern.check_distribution())
+
+if __name__ == "__main__":
+    main()
