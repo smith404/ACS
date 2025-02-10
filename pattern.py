ceb36bdfaf9b090363bb78c69972ae840e5bd846
markg <markgrahamsmith@hotmail.com>
2025-02-10 06:51:20 +0100

    added development period attribute

--- pattern.py
+++ pattern.py
@@ -12,14 +12,20 @@ class Pattern:
         for slice in self.slices:
             slice.set_duration(duration)
 
-    def delete_slice(self, pattern_slice):
+    def align_slice_periods(self, development_periods=None):
+        if development_periods is None or development_periods == 0:
+            development_periods = len(self.slices)
+        for slice in self.slices:
+            slice.set_development_periods(development_periods)
+
+    def delete_slice(self, pattern_slice=None):
         if pattern_slice in self.slices:
             self.slices.remove(pattern_slice)
         else:
             raise ValueError("PatternSlice not found in slices")
 
     def display(self):
-        print(self.pattern)
+        print(self)
         for slice in self.slices:
             slice.display()
 
@@ -33,36 +39,61 @@ class Pattern:
 
     def distribute_remaining(self):
         total_distribution = sum(slice.distribution + slice.start_distribution for slice in self.slices)
-        if (total_distribution < 1):
+        if total_distribution < 1:
             remaining = 1 - total_distribution
             for slice in self.slices:
-                slice.distribution += remaining/len(self.slices)
+                slice.distribution += remaining / len(self.slices)
+
+    def set_all_distributions_to_zero(self):
+        for slice in self.slices:
+            slice.distribution = 0
+
+    def set_all_start_distributions_to_zero(self):
+        for slice in self.slices:
+            slice.start_distribution = 0
+
+    def set_pattern_to_zero(self):
+        self.set_all_start_distributions_to_zero()
+        self.set_all_distributions_to_zero()
 
 class PatternSlice:
-    def __init__(self, distribution, duration, start_distribution):
+    def __init__(self, duration, distribution=0, start_distribution=0, development_periods=0):
         self.distribution = distribution
         self.duration = duration
         self.start_distribution = start_distribution
+        self.development_periods = development_periods
 
     def set_duration(self, duration):
         self.duration = duration
 
+    def set_development_periods(self, development_periods):
+        self.development_periods = development_periods
+
     def display(self):
-        print(f"PatternSlice: (Duration: {self.duration}, Distribution: {self.distribution}, Start Distribution: {self.start_distribution})")
+        print(self)
 
     def __str__(self):
-        return f"PatternSlice: (Duration: {self.duration}, Distribution: {self.distribution}, Start Distribution: {self.start_distribution})"
+        return f"PatternSlice: (Duration: {self.duration}, Distribution: {self.distribution}, Start Distribution: {self.start_distribution}, Development Periods: {self.development_periods})"
 
 def main():
     pattern = Pattern()
-    pattern.add_slice(PatternSlice(0.2, 90, 0.15))
-    pattern.add_slice(PatternSlice(0.15, 90, 0))
-    pattern.add_slice(PatternSlice(0.1, 90, 0))
-    pattern.add_slice(PatternSlice(0.15, 90, 0))
+    pattern.add_slice(PatternSlice(90, 0.2, 0.15))
+    pattern.add_slice(PatternSlice(90))
+    pattern.add_slice(PatternSlice(90))
+    pattern.add_slice(PatternSlice(90))
 
     pattern.expand()
     print("Distribution check:", pattern.check_distribution())
     pattern.distribute_remaining()
+    pattern.expand()
+    print("Distribution check:", pattern.check_distribution())
+    pattern.set_all_distributions_to_zero()
+    pattern.distribute_remaining()
+    pattern.expand()
+    print("Distribution check:", pattern.check_distribution())
+    pattern.set_pattern_to_zero()
+    pattern.distribute_remaining()
+    pattern.align_slice_periods()
     pattern.expand()
     print("Distribution check:", pattern.check_distribution())
 
