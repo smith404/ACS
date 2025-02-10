06596bbac3f1fa78a9e79809f31cb3a1461b4ad0
markg <markgrahamsmith@hotmail.com>
2025-02-10 14:39:41 +0100

    added offset members

--- pattern.py
+++ pattern.py
@@ -1,6 +1,7 @@
 class Pattern:
-    def __init__(self):
+    def __init__(self, duration=0):
         self.slices = []
+        self.duration = duration
 
     def add_slice(self, pattern_slice):
         if isinstance(pattern_slice, PatternSlice):
@@ -9,14 +10,17 @@ class Pattern:
             raise TypeError("Expected a PatternSlice instance")
 
     def set_duration(self, duration):
+        self.duration = duration
         for slice in self.slices:
             slice.set_duration(duration)
 
     def align_slice_periods(self, development_periods=None):
         if development_periods is None or development_periods == 0:
             development_periods = len(self.slices)
-        for slice in self.slices:
+        for index, slice in enumerate(self.slices, start=0):
             slice.set_development_periods(development_periods)
+            slice.set_duration_offset(self.duration / development_periods)
+            slice.set_start_offset(index * (self.duration / development_periods))
 
     def delete_slice(self, pattern_slice=None):
         if pattern_slice in self.slices:
@@ -32,15 +36,11 @@ class Pattern:
     def expand(self):
         for index, slice in enumerate(self.slices, start=1):
             # Check for a start distribution
-            if (slice.start_distribution != 0):
-                print(f"Slice {index}: {slice.start_distribution}")
+            if slice.start_distribution != 0:
+                slice.iterate_start_periods()
             # Check for a distribution
-            if (slice.distribution != 0):
-                print(f"Slice {index}: {slice.distribution}")
-
-    def check_distribution(self):
-        total_distribution = sum(slice.distribution + slice.start_distribution for slice in self.slices)
-        return total_distribution == 1
+            if slice.distribution != 0:
+                slice.iterate_development_periods()
 
     def distribute_remaining(self):
         total_distribution = sum(slice.distribution + slice.start_distribution for slice in self.slices)
@@ -61,18 +61,33 @@ class Pattern:
         self.set_all_start_distributions_to_zero()
         self.set_all_distributions_to_zero()
 
+    def check_distribution(self):
+        total_distribution = sum(slice.distribution + slice.start_distribution for slice in self.slices)
+        return total_distribution == 1
+
+    def check_durations(self):
+        return all(slice.duration == self.duration for slice in self.slices)
+
     def __str__(self):
-        return f"Pattern with {len(self.slices)} slices"
+        return f"Pattern with {len(self.slices)} slices and duration {self.duration}"
 
 class PatternSlice:
-    def __init__(self, duration, distribution=0, start_distribution=0, development_periods=0):
+    def __init__(self, duration, distribution=0, start_distribution=0, development_periods=0, start_offset=0, duration_offset=0):
         self.distribution = distribution
         self.duration = duration
         self.start_distribution = start_distribution
         self.development_periods = development_periods
+        self.start_offset = start_offset
+        self.duration_offset = duration_offset
 
     def set_duration(self, duration):
         self.duration = duration
+
+    def set_start_offset(self, start_offset):
+        self.start_offset = start_offset
+
+    def set_duration_offset(self, duration_offset):
+        self.duration_offset = duration_offset
 
     def set_development_periods(self, development_periods):
         self.development_periods = development_periods
@@ -80,23 +95,40 @@ class PatternSlice:
     def display(self):
         print(self)
 
+    def iterate_development_periods(self):
+        for index in range(0, self.development_periods + 1):
+            factor = self.development_periods
+            if index == 0 or index == self.development_periods:
+                factor = factor * 2
+            print(f"{self.distribution/factor}", end='\t')
+        # For a new line after the loop
+        print()
+
+    def iterate_start_periods(self):
+        for _ in range(self.development_periods):
+            print(self.start_distribution, end='\t')
+        # For a new line after the loop
+        print()
+
     def __str__(self):
-        return f"PatternSlice: (Duration: {self.duration}, Distribution: {self.distribution}, Start Distribution: {self.start_distribution}, Development Periods: {self.development_periods})"
+        return f"PatternSlice: (Duration: {self.duration}, Distribution: {self.distribution}, Start Distribution: {self.start_distribution}, Development Periods: {self.development_periods}, Start Offset: {self.start_offset}, Duration Offset: {self.duration_offset})"
 
 def main():
-    pattern = Pattern()
-    pattern.add_slice(PatternSlice(90, 0, 0.1284))
-    pattern.add_slice(PatternSlice(90))
-    pattern.add_slice(PatternSlice(90))
-    pattern.add_slice(PatternSlice(90))
+    pattern = Pattern(360)
+    pattern.add_slice(PatternSlice(360, 0, 0.1284))
+    pattern.add_slice(PatternSlice(360))
+    pattern.add_slice(PatternSlice(360))
+    pattern.add_slice(PatternSlice(360))
 
     pattern.display()        
     print("Distribution check:", pattern.check_distribution())
+    print("Duration check:", pattern.check_durations())
 
     pattern.distribute_remaining()
     pattern.align_slice_periods()
     pattern.display()
     print("Distribution check:", pattern.check_distribution())
+    print("Duration check:", pattern.check_durations())
     pattern.expand()
 
 if __name__ == "__main__":
