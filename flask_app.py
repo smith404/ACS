b3da0be1e02191fe58969b79ae08c1585d03f4a6
Mark <markgrahamsmith@hotmail.com>
2025-02-11 21:30:09 +0100

    added pattern test page

--- flask_app.py
+++ flask_app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def home():
     return "Index Page"
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', person=name)
+@app.route('/pattern/')
+@app.route('/pattern/<name>')
+def pattern(name=None):
+    return render_template('pattern_test.html', pattern=name)
 
 @app.route('/desktop/')
 def desktop():
