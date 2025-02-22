2ee33d8aca6e4ec2e8673a4421b8669c8bc94ff4
Mark <markgrahamsmith@hotmail.com>
2025-02-22 07:40:22 +0100

    added text box for development periods

--- static/components/patternSlice.js
+++ static/components/patternSlice.js
@@ -18,12 +18,16 @@ angular.module('app').component('patternSlice', {
       <div class="col-sm-2">
         <input type="number" ng-model="$ctrl.distribution" class="form-control" />
       </div>
-      <label class="col-sm-2 col-form-label">
+      <label class="col-sm-1 col-form-label">
       Start {{$ctrl.startOffset}}
       </label>
-      <label class="col-sm-2 col-form-label">
+      <label class="col-sm-1 col-form-label">
       End {{$ctrl.startOffset + $ctrl.durationOffset}}
       </label>
+      <label class="col-sm-1 col-form-label">Development Periods:</label>
+      <div class="col-sm-1">
+        <input type="number" ng-model="$ctrl.developmentPeriods" class="form-control" />
+      </div>
     </div>
   `
 });
