44c904a25887dd230f8b34ce43bf261a6ad1d636
Mark <markgrahamsmith@hotmail.com>
2025-03-13 20:47:37 +0100

    added mapping

--- pyspark_test.py
+++ pyspark_test.py
@@ -6,8 +6,27 @@ spark = SparkSession.builder \
     .appName("PySparkExample") \
     .getOrCreate()
 
+uv_data = [("B1700", 34), ("A1100", 34), ("C1100", 45), ("A1100", 29), ("C1100", 59)]
+uv_columns = ["TOA", "UV"]
+uv_df = spark.createDataFrame(uv_data, uv_columns)
+
+uv_df.show()
+
+toa_mappings = {
+    'B1700': 3410,
+    'C1100': 2400,
+    'A1100': 1400
+}
+
+# Apply toa_mappings to update uv_df with updated TOA values
+uv_df = uv_df.withColumn("TOA", col("TOA").cast("string"))
+for key, value in toa_mappings.items():
+    uv_df = uv_df.withColumn("TOA", when(col("TOA") == key, lit(value)).otherwise(col("TOA")))
+
+uv_df.show()
+
 # Create a simple DataFrame
-data = [("John", 34, "M"), ("Alice", 34, "F"), ("Bob", 45, "M"), ("Cathy", 29, "F"), ("Cathy", 59, "F")]
+data = [("John", 37, "M"), ("Alice", 64, "F"), ("Bob", 45, "M"), ("Cathy", 29, "F"), ("Cathy", 59, "F")]
 columns = ["Name", "Age", "Gender"]
 df = spark.createDataFrame(data, columns)
 
@@ -28,7 +47,6 @@ df.show()
 
 # Group by "Gender" and calculate the average "MonthsOld"
 df_grouped = df.groupBy("Gender").agg(avg("MonthsOld").alias("AverageMonthsOld"))
-#df.groupBy("year", "sex").agg(avg("percent"), count("*"))
 
 # Show the grouped DataFrame
 df_grouped.show()
@@ -36,8 +54,17 @@ df_grouped.show()
 # Perform a simple transformation
 df_filtered = df.filter(df.CurrentAge > 30)
 
+# Set the Name in df_filtered to be "<redacted>"
+df_filtered = df_filtered.withColumn("Name", lit("<redacted>"))
+
 # Show the transformed DataFrame
 df_filtered.show()
+
+# Sort df_filtered by MonthsOld from oldest to youngest
+df_filtered_sorted = df_filtered.orderBy(col("MonthsOld").desc())
+
+# Show the sorted DataFrame
+df_filtered_sorted.show()
 
 # Stop the SparkSession
 spark.stop()
