ccf8d70846c9c5d219046fafb5b8d6415c0dc0dd
Mark <markgrahamsmith@hotmail.com>
2025-03-15 15:37:51 +0100

    Rewrite the FrameMapper class

--- frame_mapper.py
+++ frame_mapper.py
@@ -1,230 +1,45 @@
 import argparse
-import pandas as pd
 from enum import Enum
-from duckdb_wrapper import DuckDBWrapper  # Import DuckDBWrapper
 import json  # Import json module
+import yaml  # Import yaml module
+import uuid  # Import uuid module
+from datetime import datetime  # Import datetime module
 from config import database_path
 
-class TransformationActionType(Enum):
-    RENAME = "rename"
-    SIMPLE_MAP = "simple_map"
-    REFORMAT = "reformat"
-    REMOVE = "remove"
-
-class Mapping:
-    def __init__(self, map_id: str, map_name: str, map_description: str, from_asset_id: str, to_asset_id: str, target_attr_value_id: str):
-        self.map_id = map_id
-        self.map_name = map_name
-        self.map_description = map_description
-        self.from_asset_id = from_asset_id
-        self.to_asset_id = to_asset_id
-        self.target_attr_value_id = target_attr_value_id
-
-    def describe(self) -> str:
-        return json.dumps({
-            'map_name': self.map_name,
-            'map_description': self.map_description,
-            'from_asset': self.from_asset_id,
-            'to_asset': self.to_asset_id,
-            'target_attr_value_id': self.target_attr_value_id
-        })
-
-    def to_json(self) -> str:
-        return json.dumps({
-            'map_id': self.map_id,
-            'map_name': self.map_name,
-            'map_description': self.map_description,
-            'from_asset_id': self.from_asset_id,
-            'to_asset_id': self.to_asset_id,
-            'target_attr_value_id': self.target_attr_value_id
-        })
-
-    @classmethod
-    def from_json(cls, json_str: str):
-        data = json.loads(json_str)
-        return cls(
-            map_id=data.get('map_id'),
-            map_name=data.get('map_name'),
-            map_description=data.get('map_description', ''),
-            from_asset_id=data.get('from_asset_id'),
-            to_asset_id=data.get('to_asset_id'),
-            target_attr_value_id=data.get('target_attr_value_id')
-        )
-
-    def __str__(self):
-        return f"Mapper(ID: {self.map_id}, Name: {self.map_name}, Description: {self.map_description}, From Asset ID: {self.from_asset_id}, To Asset ID: {self.to_asset_id}, Target Attr Value ID: {self.target_attr_value_id})"
-
-class DataAsset:
-    def __init__(self, data_asset_id: str, domain: str, asset_name: str, asset_description: str = None, medallion_layer: str = "gold", container: str = None, feed_path: str = None):
-        self.data_asset_id = data_asset_id
-        self.domain = domain
-        self.asset_name = asset_name
-        self.asset_description = asset_description
-        self.medallion_layer = medallion_layer
-        self.container = container
-        self.feed_path = feed_path
-
-    def to_json(self) -> str:
-        return json.dumps({
-            'data_asset_id': self.data_asset_id,
-            'domain': self.domain,
-            'asset_name': self.asset_name,
-            'asset_description': self.asset_description,
-            'medallion_layer': self.medallion_layer,
-            'container': self.container,
-            'feed_path': self.feed_path
-        })
-
-    @classmethod
-    def from_json(cls, json_str: str):
-        data = json.loads(json_str)
-        return cls(
-            data_asset_id=data.get('data_asset_id'),
-            domain=data.get('domain'),
-            asset_name=data.get('asset_name'),
-            asset_description=data.get('asset_description', ''),
-            medallion_layer=data.get('medallion_layer', 'gold'),
-            container=data.get('container', ''),
-            feed_path=data.get('feed_path', '')
-        )
-
-    def __str__(self):
-        return f"DataAsset(ID: {self.data_asset_id}, Domain: {self.domain}, Name: {self.asset_name}, Description: {self.asset_description}, Layer: {self.medallion_layer}, Container: {self.container}, Path: {self.feed_path})"
-
-class DataAttribute:
-    def __init__(self, data_attribute_id: str, attribute_name: str, attribute_description: str, attribute_type: str = "string", attribute_length: int = 255, external_source: str = None):
-        self.data_attribute_id = data_attribute_id
-        self.attribute_name = attribute_name
-        self.attribute_description = attribute_description
-        self.attribute_type = attribute_type
-        self.attribute_length = attribute_length
-        self.external_source = external_source
-
-    def to_json(self) -> str:
-        return json.dumps({
-            'data_attribute_id': self.data_attribute_id,
-            'attribute_name': self.attribute_name,
-            'attribute_description': self.attribute_description,
-            'attribute_type': self.attribute_type,
-            'attribute_length': self.attribute_length,
-            'external_source': self.external_source
-        })
-
-    @classmethod
-    def from_json(cls, json_str: str):
-        data = json.loads(json_str)
-        return cls(
-            data_attribute_id=data.get('data_attribute_id'),
-            attribute_name=data.get('attribute_name'),
-            attribute_description=data.get('attribute_description', ''),
-            attribute_type=data.get('attribute_type', 'string'),
-            attribute_length=data.get('attribute_length', 255),
-            external_source=data.get('external_source', '')
-        )
-
-    def __str__(self):
-        return f"DataAttribute(ID: {self.data_attribute_id}, Name: {self.attribute_name}, Description: {self.attribute_description}, Type: {self.attribute_type}, Length: {self.attribute_length}, Source: {self.external_source})"
-
-class MappingRule:
-    def __init__(self, rule_id: str, rule_name: str, rule_description: str, rule_type: str, target_attr_value_id: str, target_attr_value_value: str, valid_from: str, valid_to: str):
-        self.rule_id = rule_id
-        self.rule_name = rule_name
-        self.rule_description = rule_description
-        self.rule_type = rule_type
-        self.target_attr_value_id = target_attr_value_id
-        self.target_attr_value_value = target_attr_value_value
-        self.valid_from = valid_from
-        self.valid_to = valid_to
-
-    def parts(self) -> dict:
-        return {
-            'rule_id': self.rule_id,
-            'rule_name': self.rule_name,
-            'rule_description': self.rule_description,
-            'rule_type': self.rule_type,
-            'target_attr_value_id': self.target_attr_value_id,
-            'target_attr_value_value': self.target_attr_value_value,
-            'valid_from': self.valid_from,
-            'valid_to': self.valid_to
-        }
-    
-    def to_json(self) -> str:
-        return json.dumps({
-            'rule_id': self.rule_id,
-            'rule_name': self.rule_name,
-            'rule_description': self.rule_description,
-            'rule_type': self.rule_type,
-            'target_attr_value_id': self.target_attr_value_id,
-            'target_attr_value_value': self.target_attr_value_value,
-            'valid_from': self.valid_from,
-            'valid_to': self.valid_to
-        })
-
-    @classmethod
-    def from_json(cls, json_str: str):
-        data = json.loads(json_str)
-        return cls(
-            rule_id=data.get('rule_id'),
-            rule_name=data.get('rule_name'),
-            rule_description=data.get('rule_description', ''),
-            rule_type=data.get('rule_type', ''),
-            target_attr_value_id=data.get('target_attr_value_id', ''),
-            target_attr_value_value=data.get('target_attr_value_value', ''),
-            valid_from=data.get('valid_from', ''),
-            valid_to=data.get('valid_to', '')
-        )
-
-    def __str__(self):
-        return f"MappingRule(ID: {self.rule_id}, Name: {self.rule_name}, Description: {self.rule_description}, Type: {self.rule_type}, Target Attr Value ID: {self.target_attr_value_id}, Target Attr Value: {self.target_attr_value_value}, Valid From: {self.valid_from}, Valid To: {self.valid_to})"
-
 class FrameMapper:
-    def __init__(self, df: pd.DataFrame):
-        self.df = df
+    def __init__(self, mapper, uuid_str=None, cob=None):
+        self.mapper = mapper
+        self.uuid = uuid_str if uuid_str else str(uuid.uuid4())
+        self.cob = cob if cob else datetime.now().strftime('%Y%m%d')
+        self.load_config()
+        self.load_mapper()
 
-    def transform(self) -> pd.DataFrame:
-        # Apply transformation rules here
-        transformed_df = self.df.copy()
-        # Example transformation: Add a new column 'new_col' with values doubled from 'existing_col'
-        if 'existing_col' in transformed_df.columns:
-            transformed_df['new_col'] = transformed_df['existing_col'] * 2
-        return transformed_df
+    def load_config(self):
+        with open('config.yaml', 'r') as file:
+            self.config = yaml.safe_load(file)
+        self.mapper_directory = self.config.get('mapper_directory', '')
+
+    def load_mapper(self):
+        if not self.mapper.endswith('.json'):
+            self.mapper += '.json'
+        mapper_path = f"{self.mapper_directory}/{self.mapper}"
+        with open(mapper_path, 'r') as file:
+            self.mapping = json.load(file)
+
+    def get_mapping(self):
+        return self.mapping
 
 def main():
     """
     Main method to demonstrate the usage of FrameMapper class.
     """
     parser = argparse.ArgumentParser(description="Frame Mapper Executor")
-    parser.add_argument("--map", type=str, help="The name of the mapper to extract")
-    parser.add_argument("--asset", type=str, help="The JSON string of the data asset")  # Add asset argument
+    parser.add_argument("--mapper", type=str, help="The name of the mapper to use")
     args = parser.parse_args()
 
-    db_wrapper = DuckDBWrapper(database_path)
-    db_wrapper.connect()
-    
-    if args.map:
-        rules_json = db_wrapper.execute_named_query_to_json("get_mapper_rules", parameters={"map_name": args.map})
-        for rule in json.loads(rules_json):
-            print(f"Mapper: {rule}")
-            rule_part_json = db_wrapper.execute_named_query_to_json("get_rule_parts", parameters={"rule_id": rule['rule_id']})
-            for rule_part in json.loads(rule_part_json):
-                print(f"Rule Part: {rule_part}")
-
-    if args.asset:
-        result_json = db_wrapper.execute_named_query_to_json("read_asset", parameters={"asset_name": args.asset})
-        for result in json.loads(result_json):
-            data_asset = DataAsset.from_json(json.dumps(result))
-            print(f"Data Asset: {data_asset}")
-
-    # Sample DataFrame for demonstration
-    data = {'existing_col': [1, 2, 3, 4, 5]}
-    df = pd.DataFrame(data)
-    
-    # Create FrameMapper instance and apply transformation
-    frame_mapper = FrameMapper(df)
-    transformed_df = frame_mapper.transform()
-    
-    # Print the transformed DataFrame
-    print(transformed_df)
+    if args.mapper:
+        frame_mapper = FrameMapper(args.mapper)
+        print(frame_mapper.get_mapping())
 
 if __name__ == "__main__":
     main()
--- pyspark_test.py
+++ pyspark_test.py
@@ -1,21 +1,21 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import lit, when, col, avg
+from pyspark.sql.functions import lit, when, col, avg, split
 
 # Initialize a SparkSession
 spark = SparkSession.builder \
     .appName("PySparkExample") \
     .getOrCreate()
 
-uv_data = [("B1700", 34), ("A1100", 34), ("C1100", 45), ("A1100", 29), ("C1100", 59)]
-uv_columns = ["TOA", "UV"]
+uv_data = [("B1700", 34, "Q1_2024"), ("A1100", 34, "Q2_2024"), ("C1100", 45, "Q3_2024"), ("A1100", 29, "Q4_2024"), ("C1100", 59, "Q4_2024")]
+uv_columns = ["TOA", "UV", "FIN_PERIOD"]
 uv_df = spark.createDataFrame(uv_data, uv_columns)
 
 uv_df.show()
 
 toa_mappings = {
-    'B1700': 3410,
-    'C1100': 2400,
-    'A1100': 1400
+    'B1700': "3410",
+    'C1100': "2400",
+    'A1100': "1400"
 }
 
 # Apply toa_mappings to update uv_df with updated TOA values
@@ -23,18 +23,22 @@ uv_df = uv_df.withColumn("TOA", col("TOA").cast("string"))
 for key, value in toa_mappings.items():
     uv_df = uv_df.withColumn("TOA", when(col("TOA") == key, lit(value)).otherwise(col("TOA")))
 
+# Add columns quarter and year by splitting column FIN_PERIOD using the '_' character
+uv_df = uv_df.withColumn("quarter", split(col("FIN_PERIOD"), "_").getItem(0))
+uv_df = uv_df.withColumn("year", split(col("FIN_PERIOD"), "_").getItem(1))
+
 uv_df.show()
 
 # Create a simple DataFrame
-data = [("John", 37, "M"), ("Alice", 64, "F"), ("Bob", 45, "M"), ("Cathy", 29, "F"), ("Cathy", 59, "F")]
-columns = ["Name", "Age", "Gender"]
+data = [("John", 37, "M", "3410", "Q1_2024"), ("Alice", 64, "F", "1400", "Q2_2024"), ("Bob", 45, "M", "1400", "Q3_2024"), ("Cathy", 29, "F", "3410", "Q4_2024"), ("Cathy", 59, "F", "2400", "Q4_2024")]
+columns = ["Name", "Age", "Gender", "TOA", "FIN_PERIOD"]
 df = spark.createDataFrame(data, columns)
 
 # Rename the column "Age" to "CurrentAge"
 df = df.withColumnRenamed("Age", "CurrentAge")
 
-# Add a new column "CityOfBirth" with the value "Middlesbrough"
-df = df.withColumn("CityOfBirth", lit("Middlesbrough"))
+# Add a new column "CityOfBirth" with the value "Zürich"
+df = df.withColumn("CityOfBirth", lit("Zürich"))
 
 # Replace the name "Cathy" with "Catherine" if the age is older than 40
 df = df.withColumn("Name", when((df.Name == "Cathy") & (df.CurrentAge > 40), "Catherine").otherwise(df.Name))
@@ -65,6 +69,17 @@ df_filtered_sorted = df_filtered.orderBy(col("MonthsOld").desc())
 
 # Show the sorted DataFrame
 df_filtered_sorted.show()
+
+# Join df_filtered_sorted with uv_df using columns TOA and FIN_PERIOD
+df_joined = df_filtered_sorted.join(uv_df, on=["TOA", "FIN_PERIOD"], how="inner")
+
+# Show the joined DataFrame
+df_joined.show()
+
+# Make all column names in df_joined lowercase
+df_joined = df_joined.toDF(*[c.lower() for c in df_joined.columns])
+
+df_joined.show()
 
 # Stop the SparkSession
 spark.stop()
